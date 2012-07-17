1cadf70b1ae11aa05dc27935b409638c89e3dcd9
Carl Orthlieb <github@orthlieb.com>
2012-07-16 22:17:32 -0700

    Scaled images

---
+++ spork.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+node ../Tools/tispork -aim icons ~/Dropbox/Dev/Noble-Call-Tools
+node ../Tools/tispork -aim splashes ~/Dropbox/Dev/Noble-Call-Tools
+node ../Tools/tispork -w 400 assets ~/Dropbox/Dev/Noble-Call-Tools buttongrid
